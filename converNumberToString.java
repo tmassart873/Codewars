a03e4d04432f597a6f8fb8f855764bbe8d82325c
tmassart873 <tim.massart@hotmail.com>
2021-10-11 16:40:16 +0200

    added Kata class (Codewars)

---
+++ converNumberToString.java
@@ -0,0 +1,10 @@
+// Instructions
+
+// We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+
+class Kata {
+    public static String numberToString(int num) {
+        return Integer.toString(num);
+    }
+}
